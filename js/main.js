55bf6853f52974f99ca4aede56a6b15a38854416
Петрицкий Альберт <quakush@mail.ru>
2019-07-02 14:35:52 +0700

    сортировка

--- js/main.js
+++ js/main.js
@@ -4,22 +4,42 @@
   var template = document.querySelector('#picture').content.querySelector('.picture');
   var container = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
+  var filtersImg = document.querySelector('.img-filters');
+  var popularFilterButton = document.querySelector('#filter-popular');
+  var newFilterButton = document.querySelector('#filter-new');
+  var discussedFilterButton = document.querySelector('#filter-discussed');
+
+  var imgData = [];
+
+  var clearPhoto = function () {
+    var pictures = Array.from(container.querySelectorAll('.picture'));
+    pictures.forEach(function (it) {
+      it.parentNode.removeChild(it);
+    });
+  };
+
+  var initPhoto = function (it) {
+    var photoElement = template.cloneNode(true);
+
+    photoElement.querySelector('.picture__img').src = it.url;
+    photoElement.querySelector('.picture__likes').textContent = it.likes;
+    photoElement.querySelector('.picture__comments').textContent = it.comments.length;
+    fragment.appendChild(photoElement);
+
+    return fragment;
+  };
+
+  var renderPhotos = function (arr) {
+    arr.forEach(function (it) {
+      container.appendChild(initPhoto(it));
+    });
+  };
 
   var successHandler = function (content) {
+    imgData = content;
 
-    var initPhoto = function () {
-      for (var i = 0; i < content.length; i++) {
-        var photoElement = template.cloneNode(true);
-
-        photoElement.querySelector('.picture__img').src = content[i].url;
-        photoElement.querySelector('.picture__likes').textContent = content[i].likes;
-        photoElement.querySelector('.picture__comments').textContent = content[i].comments.length;
-        fragment.appendChild(photoElement);
-      }
-      return fragment;
-    };
-
-    container.appendChild(initPhoto());
+    renderPhotos(imgData);
+    filtersImg.classList.remove('img-filters--inactive');
   };
 
   var errorHandler = function (errorMessage) {
@@ -33,6 +53,63 @@
     document.body.insertAdjacentElement('afterbegin', node);
   };
 
-  window.load(successHandler, errorHandler);
-})();
+  var onPopularFilter = window.debounce(function () {
+    if (!popularFilterButton.classList.contains('img-filters__button--active')) {
+      popularFilterButton.classList.add('img-filters__button--active');
+    }
+    discussedFilterButton.classList.remove('img-filters__button--active');
+    newFilterButton.classList.remove('img-filters__button--active');
+    clearPhoto();
+    renderPhotos(imgData);
+  });
 
+  var onNewFilter = window.debounce(function () {
+    if (!newFilterButton.classList.contains('img-filters__button--active')) {
+      newFilterButton.classList.add('img-filters__button--active');
+    }
+    popularFilterButton.classList.remove('img-filters__button--active');
+    discussedFilterButton.classList.remove('img-filters__button--active');
+
+    var POPULAR_PHOTOS_QUANTITY = 10;
+
+    var sortArray = [];
+    while (sortArray.length < POPULAR_PHOTOS_QUANTITY) {
+      var num;
+      var random = Math.floor(Math.random() * imgData.length);
+
+      if (num !== random) {
+        sortArray.push(imgData[random]);
+      }
+      num = random;
+    }
+    clearPhoto();
+    renderPhotos(sortArray);
+  });
+
+  var onDiscussFilter = window.debounce(function () {
+    if (!discussedFilterButton.classList.contains('img-filters__button--active')) {
+      discussedFilterButton.classList.add('img-filters__button--active');
+    }
+    popularFilterButton.classList.remove('img-filters__button--active');
+    newFilterButton.classList.remove('img-filters__button--active');
+
+    var commentsQuantity = imgData.slice();
+
+    var compareComments = function (a, b) {
+      return b.comments.length - a.comments.length;
+    };
+
+    commentsQuantity.sort(compareComments);
+
+    clearPhoto();
+    renderPhotos(commentsQuantity);
+  });
+
+  window.load(successHandler, errorHandler);
+
+  popularFilterButton.addEventListener('click', onPopularFilter);
+
+  newFilterButton.addEventListener('click', onNewFilter);
+
+  discussedFilterButton.addEventListener('click', onDiscussFilter);
+})();
