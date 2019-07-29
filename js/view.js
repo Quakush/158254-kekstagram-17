efdb599347a52a4753450c0fdfac948ed055de32
Петрицкий Альберт <quakush@mail.ru>
2019-07-29 15:04:39 +0700

    обнуляется массив

--- js/view.js
+++ js/view.js
@@ -12,6 +12,10 @@
   var description = view.querySelector('.social__caption');
   var closeButton = view.querySelector('.big-picture__cancel');
 
+  var removeComments = function () {
+    comments.innerHTML = '';
+  };
+
   var createComment = function (data) {
     var commentItem = document.createElement('li');
 
@@ -34,66 +38,64 @@
     comments.appendChild(commentItem);
   };
 
-  var onCommentLoaderClick = function () {
-    setCommentsView('?');
-  };
-
-  var setCommentsView = function (data) {
-    var commentsShow = data.comments.length > COMMENTS_SHOW ? COMMENTS_SHOW : data.comments.length;
-    for (var i = 0; i < commentsShow; i++) {
-      createComment(data.comments[0]);
-      data.comments.shift();
-    }
-    if (data.comments.length === 0) {
-      commentLoader.classList.add('visually-hidden');
-    }
-  };
-
-  var setDataView = function (obj) {
-    var viewData = obj;
-
-    imgView.src = obj.url;
-    likes.textContent = obj.likes;
-    commentsCount.textContent = obj.comments.length;
-    description.textContent = obj.description;
-    setCommentsView(viewData);
-  };
-
   var showView = function (data) {
+    var viewData = data;
+
+    var setDataView = function (obj) {
+
+      imgView.src = obj.url;
+      likes.textContent = obj.likes;
+      commentsCount.textContent = obj.comments.length;
+      description.textContent = obj.description;
+      setCommentsView(viewData);
+    };
+
+    var setCommentsView = function (comm) {
+      var commentsShow = comm.comments.length > COMMENTS_SHOW ? COMMENTS_SHOW : comm.comments.length;
+      for (var i = 0; i < commentsShow; i++) {
+        createComment(comm.comments[0]);
+        comm.comments.shift();
+      }
+      if (comm.comments.length === 0) {
+        commentLoader.classList.add('visually-hidden');
+      }
+    };
+
+    var onCommentLoaderClick = function () {
+      setCommentsView(viewData);
+    };
+
+    var closeView = function () {
+      removeComments();
+      view.classList.add('hidden');
+      closeButton.removeEventListener('click', onViewCloseClick);
+      closeButton.removeEventListener('keydown', onViewKeydownEnter);
+      document.removeEventListener('keydown', onViewKeydownEsc);
+      commentLoader.removeEventListener('click', onCommentLoaderClick);
+    };
+
+    var onViewKeydownEsc = function (evt) {
+      window.utils.isKeydownEsc(evt, closeView);
+    };
+
+    var onViewKeydownEnter = function (evt) {
+      window.utils.isKeydownEnter(evt, closeView);
+    };
+
+    var onViewCloseClick = function () {
+      closeView();
+    };
+
     removeComments();
     commentLoader.classList.remove('visually-hidden');
     view.classList.remove('hidden');
     counter.classList.add('visually-hidden');
     setDataView(data);
+    setCommentsView(viewData);
     closeButton.addEventListener('click', onViewCloseClick);
     closeButton.addEventListener('keydown', onViewKeydownEnter);
     document.addEventListener('keydown', onViewKeydownEsc);
     commentLoader.addEventListener('click', onCommentLoaderClick);
-  };
-
-  var removeComments = function () {
-    comments.innerHTML = '';
-  };
-
-  var closeView = function () {
-    removeComments();
-    view.classList.add('hidden');
-    closeButton.removeEventListener('click', onViewCloseClick);
-    closeButton.removeEventListener('keydown', onViewKeydownEnter);
-    document.removeEventListener('keydown', onViewKeydownEsc);
-    commentLoader.removeEventListener('click', onCommentLoaderClick)
-  };
-
-  var onViewKeydownEsc = function (evt) {
-    window.utils.isKeydownEsc(evt, closeView);
-  };
-
-  var onViewKeydownEnter = function (evt) {
-    window.utils.isKeydownEnter(evt, closeView);
-  };
-
-  var onViewCloseClick = function () {
-    closeView();
   };
 
   window.showView = showView;
